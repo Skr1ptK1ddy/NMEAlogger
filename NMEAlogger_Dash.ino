d5d4c810680a5e2861c9192065ae8815db543adc
Skr1ptK1ddy <77166946+Skr1ptK1ddy@users.noreply.github.com>
2021-02-03 18:47:44 +0100

    Database updatet

--- NMEAlogger_Dash.ino
+++ NMEAlogger_Dash.ino
@@ -1,17 +1,20 @@
 
 // Projekt NMEA Logger
 // Lino Liebegott
-// Tobias Wehmeyer
 
+
+
+//How to upload an image (200KB) from SDcard to FTP server
+//https://forum.arduino.cc/index.php?topic=376911.0
 // https://github.com/rfetick/MPU6050_light
 #include "Wire.h"
 #include <MPU6050_light.h>
 
 #include <SD.h>
 #include <Arduino.h>
-  #include <WiFi.h>
-  #include <AsyncTCP.h>
-  #include <ESPAsyncWebServer.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
 #include <ESPDash.h>
 
@@ -57,6 +60,9 @@ int temp = 0;
 // Bar Chart Instance
 
 
+/* GSM Modul comunikation  */
+byte RESET_PIN = 4; // SIM800 Restset pin
+HardwareSerial SIM800(2);
 
 
 /* WiFi Cofig */
@@ -67,10 +73,20 @@ unsigned long ulUpLastTime =0;
 //=================================
 #define SAVEPERIOD 2000 // ms
 #define UPDATEPERIOD 1000//ms
+#define GYROPERIOD 500//ms
+
+//--DB--
+sqlite3 *db;
+sqlite3_stmt *res;
+const char *tail;
+
+
 #define bufferLen 14
+
 const int chipSelect = 5;
 const int nmeaLen = 15;
 unsigned long lastTime = 0;
+unsigned long ulGyroLastTime = 0;
 int inByte, start=0;
 String nmea = "NMEA Logger by Lino Liebegott";
 String nmeaSplit[nmeaLen] ;
@@ -89,6 +105,9 @@ void setup() {
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0));
   Serial.begin(4800); // 4800 Baud Dateneingang und Konsole
+  SIM800.begin(9600, SERIAL_8N1, 16, 17);//init GSM Modul comunikation GPIO 16,17
+  
+  
   /* Start AsyncWebServer */
   server.begin();
   Serial.println(nmea);
@@ -101,6 +120,7 @@ void setup() {
   delay(1000);
   mpu.calcOffsets(true,true); // gyro and accelero
   Serial.println("Done!\n");
+ 
   
   
   Serial.print("IP Address: ");
@@ -118,7 +138,7 @@ void setup() {
     return;
   }
   Serial.println("SD Karte OK.");
-
+sqlite3_initialize();
  
 }
 
@@ -144,7 +164,7 @@ void loop() {
 
 
 
-/// Wird mit der PERIOD Ausgeführt ///
+/// Wird mit der SAVEPERIOD Ausgeführt ///
  if (now - lastTime >= SAVEPERIOD)
   {  
     lastTime = now;
@@ -156,8 +176,21 @@ void loop() {
   
   Serial.println(nmea);
   schreibeSD();
-}
 
+ int db_open = open_database("/sd/nmea_contributors.db", &db);
+   if (db_open != SQLITE_OK)
+   {
+    Serial.println("open Database faild");
+      return;
+   }
+   create_table();
+   insert_data_set("programmers", 777, "New_Set", 0);
+   sqlite3_close(db);
+
+
+  
+}
+/// Wird mit der UPDATEPERIOD Ausgeführt ///
 if (now - ulUpLastTime >= UPDATEPERIOD)
   {  
     ulUpLastTime = now;
@@ -186,6 +219,13 @@ if (now - ulUpLastTime >= UPDATEPERIOD)
   dashboard.sendUpdates();
 
 
+
+  }
+
+/// Wird mit der GYROPERIOD Ausgeführt ///
+if (now - ulGyroLastTime >= GYROPERIOD)
+  {  ulGyroLastTime = now;
+  
 /* Gyo and Accelero   */
 
     Serial.print(millis() - timer); // Time from last report
@@ -199,12 +239,15 @@ if (now - ulUpLastTime >= UPDATEPERIOD)
     Serial.print(F("GYRO      X: "));Serial.print(mpu.getGyroX());
     Serial.print("\tY: ");Serial.print(mpu.getGyroY());
     Serial.print("\tZ: ");Serial.println(mpu.getGyroZ());
+    /* Acc Angel*/
+    Serial.print(F("ACC ANGLE X: "));Serial.print(mpu.getAccAngleX());
+    Serial.print("\tY: ");Serial.println(mpu.getAccAngleY());
+    /* Angle */
+    Serial.print(F("ANGLE     X: "));Serial.print(mpu.getAngleX());
+    Serial.print("\tY: ");Serial.print(mpu.getAngleY());
+    Serial.print("\tZ: ");Serial.println(mpu.getAngleZ());
     
     timer = millis();
-
-
-    
-
   
   }
 
@@ -334,38 +377,218 @@ if (nmea.substring(3,6) == "VWR") { // $--VWR - Relative Wind Speed and Angle
     nmeaBuffer[12]= nmeaSplit[4];//AWS
      return;}  
 }
-int i =0;
+
 void schreibeSD() {
   
 // Einen Datensatz auf die SD Karte scheiben
 
-  File datei = SD.open("/Log.txt");//, FILE_WRITE);
+  File datei = SD.open("/Log.txt", FILE_WRITE);
   if (datei) {
     datei.println(nmea);
-    datei.println(i);
-    Serial.println(i);
     datei.close();
-    i+=1;
   }  
   else {
     Serial.println("Fehler: Datei konnte nicht geöffnet werden.");
    
   }
-  
  
- 
-  
   }
+
+
+//=============DB====================
+const char* data = "Result dataset...";
+static int callback(void *data, int argc, char **argv, char **column_name)
+{
+   Serial.println((const char*) data);
+   for (int i = 0; i < argc; ++i)
+   {
+      Serial.printf("%s = %s\n", column_name[i], argv[i] ? argv[i] : "NULL");
+   }
+   return 0;
+}
+char *err_msg = 0;
+
+int open_database(const String& filename, sqlite3 **db)
+{
+   // int db_status = sqlite3_open_v2(filename.c_str(), db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+   int db_status = sqlite3_open(filename.c_str(), db);
+   if (db_status == SQLITE_OK) // Testen, ob es mit der Konstanten geht
+   {
+      Serial.println("Open database succeed");
+   }
+   else
+   {
+      Serial.println("Open database failed");
+   }
+   return db_status;
+}
+
+void create_table()
+{
+  String sql = "CREATE TABLE IF NOT EXISTS programmers (id int NOT NULL PRIMARY KEY, first_name varchar(255), contributions int)";
+  int db_status = sqlite3_exec(db, sql.c_str(), callback, (void*)data, &err_msg);
+  if (db_status == SQLITE_OK)
+  {
+    Serial.println("Table programmers created");
+  }
+  else
+  {
+    Serial.println("SQL error:");
+    Serial.println(err_msg);
+    sqlite3_free(err_msg);
+  }
+}
+
+void print_dataset(const String& table_name,const int& id)
+{
+   String sql = create_select_statement(table_name, id);
+   Serial.println("Execute SQL Statement:");
+   Serial.println(sql);
+   int db_status = sqlite3_exec(db, sql.c_str(), callback, (void*)data, &err_msg);
+   if (db_status == SQLITE_OK)
+   {
+      Serial.println("SQL statement executed");
+   }
+   else
+   {
+      Serial.println("SQL error:");
+      Serial.println(err_msg);
+      sqlite3_free(err_msg);
+   }
+}
+
+void insert_data_set(const String& table_name, const int& id, const String& first_name, const int& contributions)
+{
+  String sql = create_insert_statement();
+  int db_status = sqlite3_prepare_v2(db, sql.c_str(), strlen(sql.c_str()), &res, &tail);
+  if (db_status == SQLITE_OK)
+  {
+     Serial.println("SQL statement executed");
+     sqlite3_bind_int(res, 1, id);
+     sqlite3_bind_text(res, 2, first_name.c_str(), strlen(first_name.c_str()), SQLITE_STATIC);
+     sqlite3_bind_int(res, 3, contributions);
+     if (sqlite3_step(res) != SQLITE_DONE)
+     {
+       Serial.printf("ERROR executing stmt: %s\n", sqlite3_errmsg(db));
+       sqlite3_close(db);
+       return;
+     }
+     sqlite3_clear_bindings(res);
+     db_status = sqlite3_reset(res);
+     if (db_status != SQLITE_OK)
+     {
+       sqlite3_close(db);
+       return;
+     }
+     sqlite3_finalize(res);
+  }
+  else
+  {
+     Serial.println("SQL error:");
+     Serial.println(err_msg);
+     sqlite3_close(db);
+  }
+}
+
+String create_insert_statement()
+{
+  String sql_insert;
+  sql_insert.concat("INSERT INTO programmers VALUES (?, ?, ?);");
+  return sql_insert;
+}
+
+String create_select_statement(const String& table_name, const int& id)
+{
+  String sql_select;
+  sql_select.concat("SELECT * FROM ");
+  sql_select.concat(table_name);
+  sql_select.concat(" WHERE id = ");
+  sql_select.concat(id);
+  sql_select.concat(";");
+  return sql_select;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 
+//=============GSM===============
+
+bool gsmReset() {
+  Serial.println("gsmReset");
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, 0);
+  delay(1000);
+  digitalWrite(RESET_PIN, 1);
+  delay(5000);
+  return isSIM800Available();
+}
+
+bool isSIM800Available() {
+  if (sendATCommand("AT", true) == "_TIMEOUT") {
+    return false;
+  }
+  return true;
+}
+
+void setupGPRS() {
+  Serial.println("setupGPRS");
+  sendATCommand("AT+SAPBR=3,1,\"Contype\",\"GPRS\"", true);
+  sendATCommand("AT+SAPBR=3,1,\"APN\",\"web.vodafone.de\"", true); //TODO APN einstellungen anpassen !!!!!!!!!!!
+  openGPRSConn();
+  sendATCommand("AT+SAPBR=2,1", true);
+}
+
+void openGPRSConn() {
+  sendATCommand("AT+SAPBR=1,1", true);
+};
+
+String sendATCommand(String cmd, bool waiting) {
+  Serial.println("sendATCommand");
+  String _resp = "";                                              
+  cmd.reserve(500);
+  Serial.println(cmd);                                            
+  SIM800.println(cmd);                                            
+  if (waiting) {                                                 
+    _resp = waitResponse();                                       
+   
+    if (_resp.startsWith(cmd)) {                                
+      _resp = _resp.substring(_resp.indexOf("\r", cmd.length()) + 2);
+    }
+    Serial.println(_resp);                                      
+  }
+  return _resp;                                                   
+}
+
+String waitResponse() {                                           
+  Serial.println("waitResponse");
+  String _resp = "";                                             
+  long _timeout = millis() + 10000;                               
+  while (!SIM800.available() && millis() < _timeout)  {};        
+  if (SIM800.available()) {                                       
+    _resp = SIM800.readString();                                 
+  }
+  else {                                                         
+    Serial.println("Timeout...");                                
+    return "_TIMEOUT";
+  }
+  return _resp;                                                  
+}
 
 
 
 
 
-
-     
- 
 
 
      
